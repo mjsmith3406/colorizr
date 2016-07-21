b6225d930ce880020286169a5273e09a1c26ae4f
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-21 11:01:39 -0500

    Add create colors method and tested that it is working.

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,25 +1,30 @@
 require 'colorizr'
 
-@color_array = [{color: :red, number: 31}, {color: :green, number: 32}, {color: :yellow, number: 33},
+@@color_array = [{color: :red, number: 31}, {color: :green, number: 32}, {color: :yellow, number: 33},
   {color: :blue, number: 34}, {color: :pink, number: 36}, {color: :light_blue, number: 94},
   {color: :white, number: 97}, {color: :light_gray, number: 37}, {color: :black, number: 30}]
 
-
-def colors
-  color_array.each do |color|
-    puts color
-  end
-end
-
 class String
-  def sample_colors
-    puts "\e[#{:number}mThis is #{:color}.\e[0m"
+  def self.colors
+    color_array.each do |color|
+      puts color[:color]
+    end
   end
-end
 
-class String
+  def self.sample_colors
+    @@color_array.each do |color|
+      puts "\e[#{color[:number]}mThis is #{color[:color]}.\e[0m"
+    end
+  end
   def self.create_colors
-    
+    @@color_array.each do |color|
+      method_text = %Q{
+        def #{color[:color]}
+          "\e[#{color[:number]}m" + self + "\e[0m"
+        end
+      }
+      class_eval(method_text)
+    end
   end
 end
 String.create_colors
