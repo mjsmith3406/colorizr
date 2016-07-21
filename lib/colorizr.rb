a88cc78077cf04465ffd18657e3b5ac9bb03ecf3
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-20 22:26:41 -0500

    added colors array

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,1 +1,5 @@
 require 'colorizr'
+
+colors = [{color: red, number: 31}, {color: green, number: 32}, {color: yellow, number: 33},
+  {color: blue, number: 34}, {color: pink, number: 36}, {color: light_blue, number: 94},
+  {color: white, number: 97}, {color: light_gray, number: 37}, {color: black, number: 30}]
