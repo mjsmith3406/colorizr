481f07bd1f7e1ae6fdeed2ae6839525a79ce5548
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-21 15:36:03 -0500

    fixed bug ready to turn in

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,12 +1,14 @@
 class String
   @@color_array = [{color: :red, number: 31}, {color: :green, number: 32}, {color: :yellow, number: 33},
-    {color: :blue, number: 34}, {color: :pink, number: 36}, {color: :light_blue, number: 94},
+    {color: :blue, number: 34}, {color: :pink, number: 35}, {color: :light_blue, number: 94},
     {color: :white, number: 97}, {color: :light_gray, number: 37}, {color: :black, number: 30}]
 
   def self.colors
+    colors = []
     @@color_array.each do |color|
-      puts color[:color]
+      colors << color[:color]
     end
+    colors
   end
 
   def self.sample_colors
