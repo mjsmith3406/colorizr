34713d78305e2bd322e4fb3a89a9f96f8e5dfab4
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-21 08:10:59 -0500

    Fixed color_array for right output

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,8 +1,8 @@
 require 'colorizr'
 
-@color_array = [{color: "red", number: 31}, {color: "green", number: 32}, {color: "yellow", number: 33},
-  {color: "blue", number: 34}, {color: "pink", number: 36}, {color: "light_blue", number: 94},
-  {color: "white", number: 97}, {color: "light_gray", number: 37}, {color: "black", number: 30}]
+@color_array = [{color: :red, number: 31}, {color: :green, number: 32}, {color: :yellow, number: 33},
+  {color: :blue, number: 34}, {color: :pink, number: 36}, {color: :light_blue, number: 94},
+  {color: :white, number: 97}, {color: :light_gray, number: 37}, {color: :black, number: 30}]
 
 
 def colors
