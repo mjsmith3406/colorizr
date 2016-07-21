c450ed87751a9fedd54d74c123ad484b8fa5314d
mjsmith3406 <mjsmith3406@outlook.com>
2016-07-20 23:03:11 -0500

    Fixed bug in color array. Sample colors work. self.create_colors needs to be done, and readme

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,5 +1,25 @@
 require 'colorizr'
 
-colors = [{color: red, number: 31}, {color: green, number: 32}, {color: yellow, number: 33},
-  {color: blue, number: 34}, {color: pink, number: 36}, {color: light_blue, number: 94},
-  {color: white, number: 97}, {color: light_gray, number: 37}, {color: black, number: 30}]
+@color_array = [{color: "red", number: 31}, {color: "green", number: 32}, {color: "yellow", number: 33},
+  {color: "blue", number: 34}, {color: "pink", number: 36}, {color: "light_blue", number: 94},
+  {color: "white", number: 97}, {color: "light_gray", number: 37}, {color: "black", number: 30}]
+
+
+def colors
+  color_array.each do |color|
+    puts color
+  end
+end
+
+class String
+  def sample_colors
+    puts "\e[#{:number}mThis is #{:color}.\e[0m"
+  end
+end
+
+class String
+  def self.create_colors
+    
+  end
+end
+String.create_colors
